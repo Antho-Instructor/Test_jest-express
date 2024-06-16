a87e0f546d980182e89734f831029faaaa751e51
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-06-16 11:22:17 +0200

    ✨ (app.js): add new endpoint /api/wilders to return a list of wilders with their ids and names

--- src/app.js
+++ src/app.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello, world!");
+app.use(express.json());
+
+const wilders = [
+	{
+		id: 1,
+		name: "Yavuz",
+	},
+	{
+		id: 2,
+		name: "Ayoub",
+	},
+	{
+		id: 3,
+		name: "Benoît",
+	},
+	{
+		id: 4,
+		name: "Anthony",
+	},
+];
+
+app.get("/api/wilders", (req, res) => {
+	res.status(200).json(wilders);
 });
 
 module.exports = app;
